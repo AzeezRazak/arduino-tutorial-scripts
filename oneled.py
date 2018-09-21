804c4c0d87916d7580fc1e54416a6f7f38a668ef
Abdul Azeez <azeezshahrizal@gmail.com>
2018-09-21 18:26:56 +0800

    Create oneled.py

---
+++ oneled.py
@@ -0,0 +1,13 @@
+int blinker = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(blinker, OUTPUT);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(blinker, LOW);
+  
+}
